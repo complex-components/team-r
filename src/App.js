62dbf086dbe6fbb41dd64658a873625c5f7d71c5
Tor Oskar Wilhelmsen <toroskar.wilhelmsen@knowit.no>
2017-04-28 12:46:02 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -11,10 +11,23 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Team R!</h1>
         </div>
+        <div className="overskrift">Dagsaktuell 3 på rad</div>
         <div className="component-test">
-            <Block value="y"/><Block value="y"/><Block value="y"/><br/>
-            <Block value="y"/><Block value="y"/><Block value="y"/><br/>
-            <Block value="y"/><Block value="y"/><Block value="y"/><br/>
+            <div className="brett" >
+                <div className="rad">
+                    <Block value="y"/><Block value="y"/><Block value="y"/><Block value="y"/><Block value="y"/>
+                 </div>
+                <div className="rad">
+                    <Block value="y"/><Block value="y"/><Block value="y"/><Block value="y"/><Block value="y"/>
+                </div>
+                <div className="rad">
+                    <Block value="y"/><Block value="y"/><Block value="y"/><Block value="y"/><Block value="y"/>
+                </div>
+                 <div className="rad">
+                     <Block value="y"/><Block value="y"/><Block value="y"/><Block value="y"/><Block value="y"/>
+                 </div>
+
+            </div>
         </div>
       </div>
     );
--- src/Block.js
+++ src/Block.js
@@ -7,7 +7,7 @@ import React, { Component } from 'react';
 class Block extends Component {
     render() {
         return (
-            <div>
+            <div className="brikke">
                 <p>&nbsp;{this.props.value}&nbsp;</p>
             </div>
         );
